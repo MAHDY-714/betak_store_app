5dc51e299f06485cc09e4bba311d4d2fbe205681
mohmead mahdy <mohmedmahdy714@gmail.com>
2024-10-08 12:27:49 +0300

    refactor product Rating part 9

--- lib/Features/Screens/presentation/views/products/widget/product_details/part4/rating_amd_reviews/reviews_details/review_details_item_view.dart
+++ lib/Features/Screens/presentation/views/products/widget/product_details/part4/rating_amd_reviews/reviews_details/review_details_item_view.dart
@@ -1,5 +1,5 @@
 import 'package:betak_store_app/Features/Screens/data/models/product_rating_and_reviews_model/product_rating_and_reviews_model.dart';
-import 'package:betak_store_app/Features/Screens/presentation/views/products/widget/product_details/part4/rating_amd_reviews/rating_details/rating_stars_in_row_rating_widget.dart';
+import 'package:betak_store_app/Features/Screens/presentation/views/products/widget/product_details/part4/rating_amd_reviews/reviews_details/row_review_date_and_rate.dart';
 import 'package:betak_store_app/Features/Screens/presentation/views/products/widget/product_details/part5/description_text.dart';
 import 'package:betak_store_app/core/styles/decorations.dart';
 import 'package:betak_store_app/core/styles/text_styles.dart';
@@ -61,50 +61,6 @@ class ReviewDetailsItemBuilder extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class RowReviewDateAndRate extends StatelessWidget {
-  const RowReviewDateAndRate({
-    super.key,
-    required this.reviewDateTime,
-    required this.productRatingAndReviewsModel,
-    required this.index,
-  });
-
-  final String reviewDateTime;
-  final ProductRatingAndReviewsModel? productRatingAndReviewsModel;
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          reviewDateTime,
-          style: TextStyles.textStyle9.copyWith(
-              fontSize: kWidthCondtions(context,
-                  valueIsTrue: 9.0, valueIsFalse: 12.0)),
-        ),
-        const Spacer(),
-        Text(
-          '(${productRatingAndReviewsModel!.reviews![index].rating!.toDouble()})',
-          style: TextStyles.textStyle10W60.copyWith(
-              fontSize: kWidthCondtions(context,
-                  valueIsTrue: 10.0, valueIsFalse: 13.0)),
-        ),
-        const SizedBox(
-          width: 4,
-        ),
-        RateingStarsInRowRatingWidget(
-            rateInitialValue: productRatingAndReviewsModel!
-                .reviews![index].rating!
-                .toDouble(),
-            starsCount: 5,
-            starSize: kWidthCondtions(context,
-                valueIsTrue: 15.0, valueIsFalse: 18.0)),
-      ],
     );
   }
 }
